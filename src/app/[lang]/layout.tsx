3292dbf6845bc4a169a234244cf4ca80aa1487d2
MAHESH CHAUBEY <MAHESHCHAUBE333@gmail.com>
2025-09-06 16:49:59 +0000

    when ever new module gets loaded you should show some type of loder so t

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,7 +1,10 @@
+
 import { Header } from '@/components/layout/Header';
 import { Footer } from '@/components/layout/Footer';
 import type { Locale } from '@/lib/i18n-config';
 import { getDictionary } from '@/lib/dictionaries';
+import { Suspense } from 'react';
+import Loading from './loading';
 
 export default async function LocaleLayout({
   children,
@@ -14,8 +17,10 @@ export default async function LocaleLayout({
   return (
     <div className="flex min-h-screen flex-col">
       <Header lang={params.lang} dictionary={dictionary.header} />
-      <main className="flex-grow container mx-auto px-4 md:px-6">
-        {children}
+      <main className="flex flex-1 flex-col container mx-auto px-4 md:px-6">
+        <Suspense fallback={<Loading />}>
+          {children}
+        </Suspense>
       </main>
       <Footer lang={params.lang} dictionary={dictionary.footer} />
     </div>
