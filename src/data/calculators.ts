d544fe2f270d3b50d797a3df57818c97728a4cdb
MAHESH CHAUBEY <MAHESHCHAUBE333@gmail.com>
2025-09-07 08:06:25 +0000

    update about us page to use: about us.png image from public folder

--- src/data/calculators.ts
+++ src/data/calculators.ts
@@ -72,7 +72,7 @@ export const calculators: Calculator[] = [
     icon: FileText,
     link_text: 'Compare Now',
     lastModified: '2024-07-23',
-    image: '/images/Old-vs-New-Tax-Regime-Calculator.png'
+    image: '/images/Old vs New Tax Regime Calculator.png'
   },
   {
     slug: 'retirement-corpus-calculator',
@@ -92,7 +92,7 @@ export const calculators: Calculator[] = [
     icon: BadgePercent,
     link_text: 'Optimize Now',
     lastModified: '2024-07-21',
-    image: '/images/loan-optimizer-online.png'
+    image: '/images/Loan Optimization Calculator.png'
   },
   {
     slug: 'mutual-fund-overlap-calculator',
