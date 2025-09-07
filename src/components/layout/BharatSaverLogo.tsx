f4db6b11f7b7319cf4a1239a8003f7aa92ae8091
MAHESH CHAUBEY <MAHESHCHAUBE333@gmail.com>
2025-09-07 06:11:24 +0000

    logo is not correct yet, cna u tell me why it is breaking ?

--- src/components/layout/BharatSaverLogo.tsx
+++ src/components/layout/BharatSaverLogo.tsx
@@ -11,12 +11,12 @@ export function BharatSaverLogo(props: SVGProps<SVGSVGElement>) {
       {...props}
     >
       <path
-        d="M12 2L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-3z"
-        className="text-primary"
+        d="M12 22C12 22 20 18 20 12V5L12 2L4 5V12C4 18 12 22 12 22Z"
         fill="currentColor"
+        className="text-primary"
       />
       <path
-        d="M15.25 14.5C15.25 15.3284 14.5784 16 13.75 16H9.5M15.25 11.5H9.5M12.5 8.5H9.5C8.67157 8.5 8 9.17157 8 10C8 10.8284 8.67157 11.5 9.5 11.5H12.5C13.8807 11.5 15 10.3807 15 9C15 7.61929 13.8807 6.5 12.5 6.5H8"
+        d="M10.5 8.5H14.5M10.5 11.5H14.5M12.5 11.5C13.6046 11.5 14.5 10.6046 14.5 9.5C14.5 8.39543 13.6046 7.5 12.5 7.5H10.5C9.39543 7.5 8.5 8.39543 8.5 9.5V14.5L10.5 12.5"
         stroke="hsl(var(--primary-foreground))"
         strokeWidth="1.5"
         strokeLinecap="round"
