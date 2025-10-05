dff643882401c5659eeb672902e2d99c9d6904f7
MAHESH CHAUBEY <MAHESHCHAUBE333@gmail.com>
2025-10-05 17:30:55 +0000

    remove lic jeevan anad calcultor also

--- src/components/home/PopularTools.tsx
+++ src/components/home/PopularTools.tsx
@@ -17,7 +17,7 @@ type PopularToolsProps = {
 export function PopularTools({ lang, dictionary }: PopularToolsProps) {
   // Filter out the calculators that should not be on the home page.
   const popularCalculators = calculators.filter(calc => 
-    !['jeevan-utsav-calculator', 'lic-jeevan-labh-calculator', 'lic-single-premium-endowment-calculator'].includes(calc.slug)
+    !['jeevan-utsav-calculator', 'lic-jeevan-labh-calculator', 'lic-single-premium-endowment-calculator', 'lic-jeevan-anand-calculator'].includes(calc.slug)
   );
   
   const [loadingCalculator, setLoadingCalculator] = useState<string | null>(null);
